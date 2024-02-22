5da18f6b3f31d00b160a44c58687827c2304d8e1
GonzaloNuñez <gonzalonlf@hotmail.com>
2024-02-22 11:22:49 +0100

    Ejercicio Dom terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
  // Aquí tu código
+ const lista = document.getElementById('lista');
+ const agregar = document.getElementById('agregar');
+ 
+ agregar.addEventListener('click', function() {
+     let nombre = prompt('Nombre');
+     let apellidos = prompt('Apellidos');
+     let domicilio = prompt('Domicilio');
+     let estadoCivil = prompt('Estado Civil');
+ 
+     let nuevoElemento = document.createElement('li');
+     
+     let nombreTexto = document.createTextNode(`Nombre: ${nombre}`);
+     let apellidosTexto = document.createTextNode(`Apellidos: ${apellidos}`);
+     let domicilioTexto = document.createTextNode(`Domicilio: ${domicilio}`);
+     let estadoCivilTexto = document.createTextNode(`Estado Civil: ${estadoCivil}`);
+ 
+     nuevoElemento.appendChild(nombreTexto);
+     nuevoElemento.appendChild(document.createElement('br'));
+     nuevoElemento.appendChild(apellidosTexto);
+     nuevoElemento.appendChild(document.createElement('br'));
+     nuevoElemento.appendChild(domicilioTexto);
+     nuevoElemento.appendChild(document.createElement('br'));
+     nuevoElemento.appendChild(estadoCivilTexto);
+     
+     lista.appendChild(nuevoElemento);
+ });
